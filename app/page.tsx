2233a448f91c90bb3342666fc31c276ec783d4cb
chinami1025 <toda.chinami@gmail.com>
2023-06-22 00:00:03 +0900

    ✨ Homeに新しい画像を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+import Header from './components/Header/Header';
 import Nav from './components/Navigation/Nav';
+import kobe from './Images/BEKOBE_01.jpg';
 
 export default function Home() {
   return (
-    <main className='mx-0.5'>
+    <main className='flex'>
       <Nav />
+
+      <div>
+        <Header />
+        <Image src={kobe} alt='BE KOBEの画像' className='h-screen' />
+      </div>
     </main>
   );
 }
