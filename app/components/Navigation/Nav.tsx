ed73224d69ca6f263b83240f1d76e46376532325
chinami1025 <toda.chinami@gmail.com>
2023-06-21 15:59:18 +0900

    ✨ Navigationメニューを作成

---
+++ app/components/Navigation/Nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <ul className='w-64 h-screen list-none text-5xl justify-center text-center bg-purple-150  bg-opacity-25'>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>CAREER</li>
+          <li>SKILL</li>
+          <li>WORK</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
