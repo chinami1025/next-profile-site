c5a484aff43491b4a5e6ecaa6af255892f96ea76
chinami1025 <toda.chinami@gmail.com>
2023-06-22 00:25:40 +0900

    ✨ Navigationのbuttonに対してアニメーションを追加

--- app/components/Navigation/Nav.tsx
+++ app/components/Navigation/Nav.tsx
@@ -1,15 +1,28 @@
+import Link from 'next/link';
 import React from 'react';
+import classes from './nav.module.css';
 
 const Nav = () => {
+  const NAV_ITEMS = [
+    { href: '/', label: 'HOME' },
+    { href: '/profile', label: 'PROFILE' },
+    { href: '/career', label: 'CAREER' },
+    { href: '/skill', label: 'SKILL' },
+    { href: '/work', label: 'WORK' },
+  ];
   return (
     <div>
-      <nav>
-        <ul className='w-64 h-screen list-none text-5xl justify-center text-center bg-purple-150  bg-opacity-25'>
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>CAREER</li>
-          <li>SKILL</li>
-          <li>WORK</li>
+      <nav className='border-r border-solid border-current py-32'>
+        <ul
+          className={`${classes.btn} w-64 h-screen list-none text-5xl items-center bg-purple-150  bg-opacity-25 justify-items-center grid`}
+        >
+          {NAV_ITEMS.map((item) => {
+            return (
+              <Link key={item.href} href={item.href} className='cursor-pointer'>
+                {item.label}
+              </Link>
+            );
+          })}
         </ul>
       </nav>
     </div>
