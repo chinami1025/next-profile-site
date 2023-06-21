201c9c171d19d830f5946b3fa12a8af5cdcedbb2
chinami1025 <toda.chinami@gmail.com>
2023-06-21 15:58:11 +0900

    ✨ Headerを作成

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <title className='w-full p-40'>Welcom to Chinami PortfolioSite</title>
+    </div>
+  );
+};
+
+export default Header;
