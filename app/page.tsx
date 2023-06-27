32f2b227bb1e8e36c11bacd5f1ca4292af765656
chinami1025 <toda.chinami@gmail.com>
2023-06-27 17:30:47 +0900

    💄 ホーム画面の写真を変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Header from './components/Header/Header';
 import Nav from './components/Navigation/Nav';
-import kobe from './Images/BEKOBE_01.jpg';
+import kobe from './Images/BEKOBE.jpg';
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
           }
           title={'Welcom to Chinami PortfolioSite'}
         />
-        <Image src={kobe} alt='BE KOBEの画像' className='h-screen' />
+        <Image src={kobe} alt='BEKOBEの画像' className='h-screen' />
       </div>
     </main>
   );
