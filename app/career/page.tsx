65a23c107272a346fccb383241c88dc9492b0261
chinami1025 <toda.chinami@gmail.com>
2023-06-29 21:21:15 +0900

    ✨ Timelineを作成し、CAREERに追加

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Nav from '../components/Navigation/Nav';
 import Header from '../components/Header/Header';
+import Timeline from '../components/Timeline/Timeline';
 
 const page = () => {
   return (
     <div className='flex'>
       <Nav />
-      <Header title={'CAREER'} />
+
+      <div className='w-full h-full '>
+        <Header title={'CAREER'} />
+        <Timeline />
+      </div>
     </div>
   );
 };
