0aef8cb4308354b24bb86aa1d95b1de285332956
chinami1025 <toda.chinami@gmail.com>
2023-06-30 18:39:56 +0900

    💄 PORTFOLIOの文章と画像のリンクを変更

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,48 +1,52 @@
 import React from 'react';
 import Nav from '../components/Navigation/Nav';
 import Header from '../components/Header/Header';
-import Image from 'next/image';
-import kobe from '../Images/Slideshow/BEKOBE.jpg';
 import Link from 'next/link';
 import classes from './portfolio.module.css';
+import Image from 'next/image';
+import WCBCafe from '../Images/portfolio/WCBCafe.jpg';
+import portfolioSite from '../Images/portfolio/portfolioSite.jpg';
+import shientethou from '../Images/portfolio/shientethou.jpg';
+import ToDo from '../Images/portfolio/ToDo.jpg';
 
 const page = () => {
   const PORTFOLIO_ITEMS = [
     {
-      link: 'https://github.com/chinami1025/SupportHandbook',
-      src: kobe,
-      alt: 'kobe',
-      title: '支援手帳',
-      reason: '息子のため',
-      period: '2ヶ月',
-      language: 'Java',
+      link: 'https://chinami1025.github.io/WCBCafe/',
+      src: WCBCafe,
+      alt: 'WCBCafe',
+      title: 'WCBCafe',
+      reason: 'HTML・CSSを学習するために本を参考に作成しました。',
+      period: '１ヶ月',
+      language: 'HTML・CSS',
+    },
+    {
+      link: 'https://chinami-t-portfolio.com/',
+      src: portfolioSite,
+      alt: 'portfolioSite',
+      title: '旧ポートフォリオサイト',
+      reason: '就職活動のために作成しました。',
+      period: '１週間',
+      language: 'React',
     },
     {
       link: 'https://github.com/chinami1025/SupportHandbook',
-      src: kobe,
-      alt: 'kobe',
+      src: shientethou,
+      alt: 'shientethou',
       title: '支援手帳',
-      reason: '息子のため',
-      period: '2ヶ月',
+      reason:
+        '難病の息子のために\n母子手帳のようなものが欲しくて作成しました。',
+      period: '３ヶ月',
       language: 'Java',
     },
     {
-      link: 'https://github.com/chinami1025/SupportHandbook',
-      src: kobe,
-      alt: 'kobe',
-      title: '支援手帳',
-      reason: '息子のため',
-      period: '2ヶ月',
-      language: 'Java',
-    },
-    {
-      link: 'https://github.com/chinami1025/SupportHandbook',
-      src: kobe,
-      alt: 'kobe',
-      title: '支援手帳',
-      reason: '息子のため',
-      period: '2ヶ月',
-      language: 'Java',
+      link: 'https://chinami1025.github.io/react-todo-app/',
+      src: ToDo,
+      alt: 'ToDo',
+      title: 'ToDoリスト',
+      reason: 'Reactを学習するために動画を参考に作成しました。',
+      period: '１週間',
+      language: 'React',
     },
   ];
 
@@ -60,14 +64,14 @@ const page = () => {
               >
                 <Link href={item.link} target='_blank'>
                   <Image
-                    src={kobe}
+                    src={item.src}
                     alt={`${item.alt}の画像`}
                     className='w-full h-full rounded-2xl mb-4 object-cover'
                   />
                 </Link>
                 <div className='w-full h-1/2'>
                   <p className='text-3xl pb-4'>{item.title}</p>
-                  <ul className='text-start text-xl'>
+                  <ul className='text-start text-xl whitespace-pre-wrap leading-loose'>
                     <li>作成理由：{item.reason}</li>
                     <li>作成期間：{item.period}</li>
                     <li>使用言語：{item.language}</li>
