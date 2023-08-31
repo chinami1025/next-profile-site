cea3cceaa72610a286a15dc49582faa11573fdca
chinami1025 <toda.chinami@gmail.com>
2023-08-31 17:23:19 +0900

    💄 Footerの大きさを調整

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='w-full p-10 pc:text-xl text-center border-solid border-current border-t mt-5'>
+    <div className='w-full p-12 lg:text-xl text-center border-solid border-current border-t absolute  lg:bottom-0 lg:static m-0'>
       © 2023 Chinami Toda
     </div>
   );
